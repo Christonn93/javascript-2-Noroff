90dc4bbbc3c64c2804d4c2d437e465bbaffee209
Fermain <Fermain@users.noreply.github.com>
2022-10-11 13:28:28 +0200

    Removing unused variables and formatting

--- src/js/api/posts/createPost.mjs
+++ src/js/api/posts/createPost.mjs
@@ -7,42 +7,37 @@ const createNew = apiVar.createNewPost;
 
 const postUrl = url + createNew;
 
-const createPost = document.querySelector("#createPost");
-
-
 async function createPosts(postData) {
- try {
-  if (postData) {
-   let newPostData = {};
+   try {
+      if (postData) {
+         let newPostData = {};
 
-   if (postData.media === "") {
-        delete postData.media
+         if (postData.media === "") {
+            delete postData.media
+         }
+         newPostData = postData;
+         const request = await fetchApi(postUrl, "POST", localStorage.getItem("token"), newPostData);
+         console.log(request);
+      }
+   } catch (error) {
+      console.log(error)
    }
-   newPostData = postData;
-   const request = await fetchApi(postUrl, "POST", localStorage.getItem("token"), newPostData);
-   console.log(request);
-  }
-
-  //    window.location.reload();
- } catch (error) {
-    console.log(error)
- }
 }
 
 export function createPostListener() {
- const form = document.querySelector("#createPost");
+   const form = document.querySelector("#createPost");
 
- if (form) {
-  form.addEventListener("submit", (event) => {
-   event.preventDefault();
+   if (form) {
+      form.addEventListener("submit", (event) => {
+         event.preventDefault();
 
-   const form = event.target;
-   const formData = new FormData(form);
-   const newData = Object.fromEntries(formData.entries());
+         const form = event.target;
+         const formData = new FormData(form);
+         const newData = Object.fromEntries(formData.entries());
 
-   console.log("New form data", newData);
-   // send it to API
-   createPosts(newData);
-  });
- }
+         console.log("New form data", newData);
+         // send it to API
+         createPosts(newData);
+      });
+   }
 }
