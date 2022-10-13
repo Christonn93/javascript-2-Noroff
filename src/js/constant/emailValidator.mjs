a39f14adf590d8e37c36acc484801e05ce5da50f
Pjatte <91551936+Pjatte1337@users.noreply.github.com>
2022-10-13 16:22:11 +0200

    Update emailValidator.mjs

--- src/js/constant/emailValidator.mjs
+++ src/js/constant/emailValidator.mjs
@@ -1,4 +1,4 @@
 export const validateEmail = (email) => {
-  return String(email).toLowerCase().match("[\w\-.]+@(stud.)?noroff.no");
+  return String(email).toLowerCase().match("w-.]+@(stud.)?noroff.no");
 };
 
